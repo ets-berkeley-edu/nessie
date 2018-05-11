7a60655325f90de3ee436732c5a949aa534c7380
Ray Davis <raydavis@berkeley.edu>
2018-05-11 14:35:12 -0700

    BOAC-880 Fix gluttonous active_student_enrollments query

--- nessie/sql_templates/create_boac_schema.template.sql
+++ nessie/sql_templates/create_boac_schema.template.sql
@@ -217,8 +217,11 @@ AS (
         {redshift_schema_intermediate}.active_student_enrollments ase
         JOIN {redshift_schema_intermediate}.users
             ON ase.canvas_user_id = {redshift_schema_intermediate}.users.canvas_id
+        JOIN {redshift_schema_canvas}.course_dim cd
+            ON ase.canvas_course_id = cd.canvas_id
         JOIN {redshift_schema_canvas}.enrollment_fact enr
             ON enr.user_id = {redshift_schema_intermediate}.users.global_id
+              AND enr.course_id = cd.id
         JOIN {redshift_schema_canvas}.course_score_fact csf
             ON csf.enrollment_id = enr.enrollment_id
     GROUP BY
