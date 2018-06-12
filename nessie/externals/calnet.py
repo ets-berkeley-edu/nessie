90657163e216fcc087f6d01d8a40a737b5023049
John Crossman <johncrossman@berkeley.edu>
2018-06-12 16:03:52 -0700

    NS-84, initial CalNet integration

---
+++ nessie/externals/calnet.py
@@ -0,0 +1,148 @@
+"""
+Copyright ©2018. The Regents of the University of California (Regents). All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its documentation
+for educational, research, and not-for-profit purposes, without fee and without a
+signed licensing agreement, is hereby granted, provided that the above copyright
+notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions.
+
+Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue,
+Suite 510, Berkeley, CA 94720-1620, (510) 643-7201, otl@berkeley.edu,
+http://ipira.berkeley.edu/industry-info for commercial licensing opportunities.
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
+"AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+"""
+
+
+import os
+import ssl
+
+import ldap3
+from nessie.lib import mockingbird
+
+SCHEMA_DICT = {
+    'berkeleyEduAffiliations': 'affiliations',
+    'berkeleyEduCSID': 'csid',
+    'berkeleyEduOfficialEmail': 'campus_email',
+    'cn': 'sortable_name',
+    'displayName': 'name',
+    'mail': 'email',
+    'givenName': 'first_name',
+    'sn': 'last_name',
+    'uid': 'uid',
+}
+
+BATCH_QUERY_MAXIMUM = 500
+
+
+def client(app):
+    if mockingbird._environment_supports_mocks():
+        c = MockClient(app)
+    else:
+        c = Client(app)
+    return c
+
+
+class Client:
+    def __init__(self, app):
+        self.app = app
+        self.host = app.config['LDAP_HOST']
+        self.bind = app.config['LDAP_BIND']
+        self.password = app.config['LDAP_PASSWORD']
+        tls = ldap3.Tls(validate=ssl.CERT_REQUIRED)
+        server = ldap3.Server(self.host, port=636, use_ssl=True, get_info=ldap3.ALL, tls=tls)
+        self.server = server
+
+    def connect(self):
+        conn = ldap3.Connection(self.server, user=self.bind, password=self.password, auto_bind=ldap3.AUTO_BIND_TLS_BEFORE_BIND)
+        return conn
+
+    def search_csids(self, csids):
+        all_out = []
+        for i in range(0, len(csids), BATCH_QUERY_MAXIMUM):
+            csids_batch = csids[i:i + BATCH_QUERY_MAXIMUM]
+            entries = self._search_csids(csids_batch)
+            out = [_attributes_to_dict(entry) for entry in entries]
+            all_out += out
+        return all_out
+
+    def search_uids(self, uids):
+        all_out = []
+        for i in range(0, len(uids), BATCH_QUERY_MAXIMUM):
+            uids_batch = uids[i:i + BATCH_QUERY_MAXIMUM]
+            entries = self._search_uids(uids_batch)
+            out = [_attributes_to_dict(entry) for entry in entries]
+            all_out += out
+        return all_out
+
+    def _search_csids(self, csids):
+        with self.connect() as conn:
+            conn.search('ou=people,dc=berkeley,dc=edu', self._csids_filter(csids), attributes=ldap3.ALL_ATTRIBUTES)
+            entries = conn.entries
+        return entries
+
+    def _search_uids(self, uids):
+        with self.connect() as conn:
+            conn.search('ou=people,dc=berkeley,dc=edu', self._uids_filter(uids), attributes=ldap3.ALL_ATTRIBUTES)
+            entries = conn.entries
+        return entries
+
+    @classmethod
+    def _csids_filter(cls, csids):
+        clauses = ''.join(f'(berkeleyeducsid={sid})' for sid in csids)
+        return f'(&(objectclass=person)(|{clauses}))'
+
+    @classmethod
+    def _uids_filter(cls, uids):
+        clauses = ''.join(f'(uid={uid})' for uid in uids)
+        return f'(&(objectclass=person)(|{clauses}))'
+
+
+class MockClient(Client):
+    def __init__(self, app):
+        self.app = app
+        self.host = app.config['LDAP_HOST']
+        self.bind = app.config['LDAP_BIND']
+        self.password = app.config['LDAP_PASSWORD']
+        server = ldap3.Server.from_definition(self.host, _fixture_path('server_info'), _fixture_path('server_schema'))
+        self.server = server
+
+    def connect(self):
+        conn = ldap3.Connection(self.server, user=self.bind, password=self.password, client_strategy=ldap3.MOCK_SYNC)
+        conn.strategy.entries_from_json(_fixture_path('search_entries'))
+        return conn
+
+
+def _attributes_to_dict(entry):
+    out = dict.fromkeys(SCHEMA_DICT.values(), None)
+    # ldap3's entry.entry_attributes_as_dict would work for us, except that it wraps a single value as a list.
+    for attr in SCHEMA_DICT:
+        if attr in entry.entry_attributes:
+            out[SCHEMA_DICT[attr]] = entry[attr].value
+    return out
+
+
+def _create_fixtures(app, sample_csids):
+    fixture_output = os.environ.get('FIXTURE_OUTPUT_PATH') or mockingbird._get_fixtures_path()
+    cl = Client(app)
+    cl.server.info.to_file(f'{fixture_output}/calnet_server_info.json')
+    cl.server.schema.to_file(f'{fixture_output}/calnet_server_schema.json')
+    conn = cl.connect()
+    conn.search('ou=people,dc=berkeley,dc=edu', cl._csids_filter(sample_csids), attributes=ldap3.ALL_ATTRIBUTES)
+    conn.response_to_file(f'{fixture_output}/calnet_search_entries.json', raw=True)
+    conn.unbind()
+
+
+def _fixture_path(pattern):
+    fixtures_path = mockingbird._get_fixtures_path()
+    return f'{fixtures_path}/calnet_{pattern}.json'
---
+++ nessie/models/calnet.py
@@ -0,0 +1,79 @@
+"""
+Copyright ©2018. The Regents of the University of California (Regents). All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its documentation
+for educational, research, and not-for-profit purposes, without fee and without a
+signed licensing agreement, is hereby granted, provided that the above copyright
+notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions.
+
+Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue,
+Suite 510, Berkeley, CA 94720-1620, (510) 643-7201, otl@berkeley.edu,
+http://ipira.berkeley.edu/industry-info for commercial licensing opportunities.
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
+"AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+"""
+
+from flask import current_app as app
+from nessie import db, std_commit
+from nessie.externals import calnet
+from nessie.models.json_cache import stow
+from nessie.models.student import Student
+
+
+@stow('calnet_user_for_uid_{uid}')
+def get_calnet_user_for_uid(app, uid):
+    persons = calnet.client(app).search_uids([uid])
+    p = persons[0] if len(persons) > 0 else None
+    return {
+        'uid': uid,
+        'firstName': p and p['first_name'],
+        'lastName': p and p['last_name'],
+    }
+
+
+def update_student_attributes(students=None):
+    sid_map = {}
+    for student in students:
+        sid_map.setdefault(student.sid, []).append(student)
+    sids = list(sid_map.keys())
+
+    # Search LDAP.
+    all_attributes = calnet.client(app).search_csids(sids)
+    if len(sids) != len(all_attributes):
+        app.logger.warning(f'Looked for {len(sids)} SIDs but only found {len(all_attributes)}')
+
+    # Update db
+    for a in all_attributes:
+        # Since we searched LDAP by SID, we can be fairly sure that the results have SIDs.
+        sid = a['csid']
+        name_split = a['sortable_name'].split(',') if 'sortable_name' in a else ''
+        full_name = [name.strip() for name in reversed(name_split)]
+        for m in sid_map[sid]:
+            new_uid = a['uid']
+            if m.uid != new_uid:
+                app.logger.info(f'For SID {sid}, changing UID {m.uid} to {new_uid}')
+                m.uid = new_uid
+            new_first_name = full_name[0] if len(full_name) else ''
+            new_last_name = full_name[1] if len(full_name) > 1 else ''
+            if (m.first_name != new_first_name) or (m.last_name != new_last_name):
+                app.logger.info(f'For SID {sid}, changing name "{m.first_name} {m.last_name}" to "{new_first_name} {new_last_name}"')
+                m.first_name = new_first_name
+                m.last_name = new_last_name
+    return students
+
+
+def merge_student_calnet_data():
+    students = Student.query.all()
+    update_student_attributes(students)
+    app.logger.info(f'Modified {len(db.session.dirty)} student records from calnet')
+    std_commit()
