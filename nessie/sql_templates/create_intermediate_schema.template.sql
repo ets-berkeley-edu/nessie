e83e3e3435b227ce37d1e97ddd9b2468d190e314
Ray Davis <raydavis@berkeley.edu>
2018-06-14 09:09:15 -0700

    NS-108 Relocate page views logic from BOAC schema

--- nessie/sql_templates/create_intermediate_schema.template.sql
+++ nessie/sql_templates/create_intermediate_schema.template.sql
@@ -216,3 +216,138 @@ AS (
         {redshift_schema_canvas}.enrollment_dim.id,
         {redshift_schema_canvas}.enrollment_dim.last_activity_at
 );
+
+/*
+ * Approximate the Canvas API's Page Views metric using the Canvas Data loads.
+ * NOTE: This table is not currently referred to by other code, but it is used to support ongoing
+ * data research and monitoring.
+ */
+DROP TABLE IF EXISTS {redshift_schema_intermediate}.page_views_zscore;
+
+CREATE TABLE {redshift_schema_intermediate}.page_views_zscore
+AS (
+WITH
+    /*
+     * Get all active student enrollments on {redshift_schema_canvas}.
+     * The aggregation helps in de-duplication of enrollments
+     * as there can be multiple records arising due to StudentEnrollment
+     * memberships to different sections. Also, aggregation is faster than
+     * the regular DISTINCT clause as it invoke Spectrum layer
+     */
+    e1 AS (
+        SELECT
+            user_id,
+            course_id,
+            type,
+            count(*)
+        FROM
+            {redshift_schema_canvas}.enrollment_dim
+        WHERE
+            workflow_state IN ('active', 'completed')
+            AND type = 'StudentEnrollment'
+        GROUP BY
+            user_id,
+            course_id,
+            type
+    ),
+    /*
+     * Filter out requests which were unlikely to correspond to a single student page view.
+     * TODO: Add page_view and participation dictionary tables to further filter requests
+     */
+    w0 AS (
+        SELECT
+            e1.user_id,
+            e1.course_id,
+            r.url,
+            e1.type AS enrollment_type
+        FROM
+            e1
+        LEFT OUTER JOIN {redshift_schema_canvas}.requests r ON r.user_id = e1.user_id
+            AND r.course_id = e1.course_id
+            AND r.url LIKE '/courses/%'
+            AND r.url NOT LIKE '/courses/%/files/%module_item_id%'
+            AND r.url NOT LIKE '/courses/%/files/%/inline_view'
+            AND r.url NOT LIKE '/courses/%/modules/items/assignment_info'
+            AND r.url NOT LIKE '/courses/%/modules/progressions'
+    ),
+    /*
+     * Calculates activity frequency for a user on a course level
+     */
+    w1 AS (
+        SELECT
+            course_id, user_id, COALESCE(count(url), 0) AS user_page_views
+        FROM
+            w0
+        WHERE
+            course_id IS NOT NULL
+            AND user_id IS NOT NULL
+        GROUP BY
+            course_id,
+            user_id
+    ),
+    /*
+     * Calculates activity frequency for each course
+     * TODO: There are a few courses that span enrollment terms
+     * Check if the frequency is correctly calculated for them
+     */
+    w2 AS (
+        SELECT
+            course_id,
+            COALESCE(AVG(user_page_views), 0) AS avg_course_page_views,
+            CAST(STDDEV_POP(user_page_views) AS dec (14, 2)) stddev_course_page_views
+        FROM
+            w1
+        GROUP BY
+            course_id
+    ),
+    /*
+     * Calculates Z-scores for users records having non zero std dev
+     */
+    w3 AS (
+        SELECT
+            w1.course_id AS course_id,
+            w1.user_id AS user_id,
+            w1.user_page_views,
+            w2.avg_course_page_views,
+            w2.stddev_course_page_views, (w1.user_page_views - w2.avg_course_page_views) / (w2.stddev_course_page_views) AS user_page_view_zscore
+        FROM
+            w1
+            JOIN w2 ON w1.course_id = w2.course_id
+        WHERE
+            w2.stddev_course_page_views > 0.00
+    ),
+    /*
+     * page_views_zscore calculation query, where nulls/divide by zero errors are handled
+     */
+    w4 AS (
+        SELECT
+            w1.course_id AS course_id, w1.user_id AS user_id, w1.user_page_views, w2.avg_course_page_views, w2.stddev_course_page_views, COALESCE(w3.user_page_view_zscore, 0) AS user_page_view_zscore
+        FROM
+            w1
+            LEFT JOIN w3 ON w1.course_id = w3.course_id
+                AND w1.user_id = w3.user_id
+            LEFT JOIN w2 ON w1.course_id = w2.course_id
+    )
+    /*
+     * Add user and courses related information
+     * Instructure uses bigintegers internally as keys.
+     * Switch with canvas course_ids and user_ids
+     */
+    SELECT
+        w4.user_id,
+        u.canvas_id AS canvas_user_id,
+        u.sis_login_id,
+        u.sis_user_id,
+        w4.course_id,
+        c.canvas_id AS canvas_course_id,
+        c.code AS canvas_course_code,
+        c.sis_source_id AS sis_course_id,
+        w4.user_page_views,
+        w4.avg_course_page_views,
+        w4.stddev_course_page_views,
+        w4.user_page_view_zscore AS user_page_views_zscore
+    FROM
+        w4
+        LEFT JOIN {redshift_schema_intermediate}.users u ON w4.user_id = u.global_id
+        LEFT JOIN {redshift_schema_canvas}.course_dim c ON w4.course_id = c.id
+);
