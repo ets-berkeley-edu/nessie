a9a8b12e3d327af1810854c272029159410dd9bf
Paul Kerschen <kerschen@berkeley.edu>
2018-06-14 12:08:24 -0700

    NS-87 SIS enrollments API background job

--- config/default.py
+++ config/default.py
@@ -57,6 +57,8 @@ CANVAS_DATA_API_KEY = 'some key'
 CANVAS_DATA_API_SECRET = 'some secret'
 CANVAS_DATA_HOST = 'foo.instructure.com'
 
+CURRENT_TERM = 'Fall 2017'
+
 DEGREE_PROGRESS_API_URL = 'https://secreturl.berkeley.edu/PSFT_CS'
 DEGREE_PROGRESS_API_USERNAME = 'secretuser'
 DEGREE_PROGRESS_API_PASSWORD = 'secretpassword'
