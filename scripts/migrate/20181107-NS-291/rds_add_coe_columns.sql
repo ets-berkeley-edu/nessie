906c940c3b56c21fe3f135d66e86c5084ec41f38
Paul Kerschen <kerschen@berkeley.edu>
2018-11-07 11:19:51 -0800

    NS-291 NS-300 Migration script for new COE columns in RDS

---
+++ scripts/migrate/20181107-NS-291/rds_add_coe_columns.sql
@@ -0,0 +1,7 @@
+ALTER table boac_advising_coe.students
+ADD COLUMN sat1read INT,
+ADD COLUMN sat1math INT,
+ADD COLUMN sat2math INT,
+ADD COLUMN in_met BOOLEAN,
+ADD COLUMN grad_term VARCHAR,
+ADD COLUMN grad_year VARCHAR;
