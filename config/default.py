d0313db25820480d35fa006c7a98f1ec21822d73
Ray Davis <raydavis@berkeley.edu>
2019-01-28 15:28:02 -0800

    NS-336 Default EARLIEST_TERM to the value used by all servers

--- config/default.py
+++ config/default.py
@@ -81,7 +81,7 @@ DEGREE_PROGRESS_API_URL = 'https://secreturl.berkeley.edu/PSFT_CS'
 DEGREE_PROGRESS_API_USERNAME = 'secretuser'
 DEGREE_PROGRESS_API_PASSWORD = 'secretpassword'
 
-EARLIEST_TERM = 'Fall 2013'
+EARLIEST_TERM = 'Fall 2016'
 
 ENROLLMENTS_API_ID = 'secretid'
 ENROLLMENTS_API_KEY = 'secretkey'
