5b284ee01d861394ac6ea97c08e4cc8aab499d3e
Anne Lyttle <alyttle@berkeley.edu>
2019-05-24 09:59:45 -0700

    NS-546: Fixes SQL error

--- nessie/sql_templates/index_sis_advising_notes.template.sql
+++ nessie/sql_templates/index_sis_advising_notes.template.sql
@@ -88,7 +88,7 @@ INSERT INTO {rds_schema_sis_advising_notes}.advising_note_topics (
   )
 );
 
-DROP MATERIALIZED VIEW IF EXISTS {rds_schema_sis_advising_notes}.advising_note_topics CASCADE;
+DROP MATERIALIZED VIEW IF EXISTS {rds_schema_sis_advising_notes}.advising_notes_search_index CASCADE;
 
 CREATE MATERIALIZED VIEW {rds_schema_sis_advising_notes}.advising_notes_search_index AS (
   SELECT id, to_tsvector('english', note_body) AS fts_index
