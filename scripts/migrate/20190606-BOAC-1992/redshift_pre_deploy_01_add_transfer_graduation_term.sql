4f3afba67214b1f828e2f43798d458d9e750f8e4
Paul Kerschen <kerschen@berkeley.edu>
2019-06-07 11:15:16 -0700

    NS-558 Staged student schema needs migration too

--- scripts/migrate/20190606-BOAC-1992/redshift_pre_deploy_01_add_transfer_graduation_term.sql
+++ scripts/migrate/20190606-BOAC-1992/redshift_pre_deploy_01_add_transfer_graduation_term.sql
@@ -21,4 +21,21 @@ DROP TABLE student.student_academic_status;
 
 ALTER TABLE student.student_academic_status_new RENAME TO student_academic_status;
 
+DROP TABLE student_staging.student_academic_status;
+
+CREATE TABLE student_staging.student_academic_status
+(
+    sid VARCHAR NOT NULL,
+    uid VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    level VARCHAR(2),
+    gpa DECIMAL(4,3),
+    units DECIMAL (6,3),
+    transfer BOOLEAN,
+    expected_grad_term VARCHAR(4)
+)
+DISTKEY (units)
+INTERLEAVED SORTKEY (sid, last_name, level, gpa, units, uid, first_name);
+
 COMMIT TRANSACTION;
