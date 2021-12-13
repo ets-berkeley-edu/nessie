b29813bc695d02f19009f901c95e8034e16222d7
Anne Lyttle <alyttle@berkeley.edu>
2021-12-13 11:24:45 -0800

    BOAC-2216: Fills profile summary col with initial value, removes default value

--- scripts/migrate/20211209-BOAC-2216/rds_pre_deploy_01_add_column_student_profile_summary.sql
+++ scripts/migrate/20211209-BOAC-2216/rds_pre_deploy_01_add_column_student_profile_summary.sql
@@ -1,1 +1,7 @@
-ALTER TABLE student.student_profiles ADD COLUMN profile_summary TEXT NOT NULL;
+BEGIN TRANSACTION;
+
+ALTER TABLE student.student_profiles ADD COLUMN profile_summary TEXT;
+UPDATE student.student_profiles SET profile_summary = '{}';
+ALTER TABLE student.student_profiles ALTER COLUMN profile_summary SET NOT NULL;
+
+COMMIT;
--- scripts/migrate/20211209-BOAC-2216/redshift_pre_deploy_01_add_column_student_profile_summary.sql
+++ scripts/migrate/20211209-BOAC-2216/redshift_pre_deploy_01_add_column_student_profile_summary.sql
@@ -1,6 +1,29 @@
 BEGIN TRANSACTION;
 
-ALTER TABLE student.student_profiles ADD COLUMN profile_summary VARCHAR(max) NOT NULL DEFAULT '{}';
-ALTER TABLE student_staging.student_profiles ADD COLUMN profile_summary VARCHAR(max) NOT NULL DEFAULT '{}';
+CREATE TABLE IF NOT EXISTS student.student_profiles_new
+(
+    sid VARCHAR NOT NULL,
+    profile VARCHAR(max) NOT NULL,
+    profile_summary VARCHAR(max) NOT NULL
+)
+DISTKEY (sid)
+SORTKEY (sid);
+
+INSERT INTO student.student_profiles_new (SELECT sid, profile, '{}' FROM student.student_profiles);
+DROP TABLE student.student_profiles;
+ALTER TABLE student.student_profiles_new RENAME TO student_profiles;
+
+CREATE TABLE IF NOT EXISTS student_staging.student_profiles_new
+(
+    sid VARCHAR NOT NULL,
+    profile VARCHAR(max) NOT NULL,
+    profile_summary VARCHAR(max) NOT NULL
+)
+DISTKEY (sid)
+SORTKEY (sid);
+
+INSERT INTO student_staging.student_profiles_new (SELECT sid, profile, '{}' FROM student_staging.student_profiles);
+DROP TABLE student_staging.student_profiles;
+ALTER TABLE student_staging.student_profiles_new RENAME TO student_profiles;
 
 COMMIT;
