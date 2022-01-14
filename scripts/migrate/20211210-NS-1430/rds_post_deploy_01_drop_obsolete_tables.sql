9cbae08798d862d872d3c4e87425a0b4332a89c4
Pauline Kerschen <kerschen@berkeley.edu>
2022-01-14 12:24:32 -0800

    NS-1430 Update migration scripts to drop obsolete tables

---
+++ scripts/migrate/20211210-NS-1430/rds_post_deploy_01_drop_obsolete_tables.sql
@@ -0,0 +1,4 @@
+ALTER TABLE student.student_degrees DROP COLUMN hist_enr;
+ALTER TABLE student.student_profile_index DROP COLUMN hist_enr;
+
+DROP SCHEMA undergrads CASCADE;
--- scripts/migrate/20211210-NS-1430/redshift_post_deploy_01_drop_obsolete_tables.sql
+++ scripts/migrate/20211210-NS-1430/redshift_post_deploy_01_drop_obsolete_tables.sql
@@ -1,4 +1,8 @@
-DROP TABLE boac_advisee.non_current_students;
-DROP SCHEMA boac_advisee;
+DROP SCHEMA boac_advisee CASCADE;
+DROP SCHEMA boac_advising_undergrads CASCADE;
+DROP SCHEMA sis_data CASCADE;
+
+ALTER TABLE student.student_profile_index DROP COLUMN hist_enr;
+ALTER TABLE student_staging.student_profile_index DROP COLUMN hist_enr;
 DROP TABLE student.student_term_gpas;
 DROP TABLE student_staging.student_term_gpas;
