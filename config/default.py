ccac04caef7ea25c3e75290defe327f376feb6ad
Pauline Kerschen <kerschen@berkeley.edu>
2022-02-02 10:28:55 -0800

    NS-1459 Split SIS data RDS dblink into Damien/Diablo specific roles

--- config/default.py
+++ config/default.py
@@ -212,7 +212,8 @@ RDS_APP_BOA_USER = 'boa rds username'
 REDSHIFT_APP_BOA_USER = 'boa redshift username'
 
 # RDS and Redshift readonly roles for dblink user mapping
-RDS_DBLINK_ROLE_SIS_DATA = 'ro_sis_data_role'
+RDS_DBLINK_ROLE_DAMIEN = 'damien_role'
+RDS_DBLINK_ROLE_DIABLO = 'diablo_role'
 REDSHIFT_DBLINK_GROUP = 'nessie_dblink_group'
 REDSHIFT_DBLINK_GROUP_DIABLO = 'diablo_dblink_group'
 
