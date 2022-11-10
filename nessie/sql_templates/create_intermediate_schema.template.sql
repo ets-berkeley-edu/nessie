8117c344b70d0f364eff27b4ace2390a12f46557
Pauline Kerschen <kerschen@berkeley.edu>
2022-11-10 15:24:58 -0800

    DAMIEN-499 Don't screen out SIS sections associated with deleted Canvas sections

--- nessie/sql_templates/create_intermediate_schema.template.sql
+++ nessie/sql_templates/create_intermediate_schema.template.sql
@@ -184,8 +184,8 @@ AS (
         ON extracted_section_ids.sis_term_id >= '{earliest_term_id}'
         AND extracted_section_ids.sis_term_id = sc.term_id::int
         AND extracted_section_ids.sis_section_id = sc.section_id::int
-        AND (c.workflow_state IN ('available', 'completed'))
-    WHERE (s.workflow_state IS NULL OR s.workflow_state != 'deleted')
+        AND c.workflow_state IN ('available', 'completed')
+        AND (s.workflow_state IS NULL OR s.workflow_state != 'deleted')
     /* Clear out duplicates, since SIS data will contain multiple rows for multiple meetings or instructor assignments. */
     GROUP BY
         c.canvas_id, s.canvas_id, c.name, c.code, s.name, et.name,
