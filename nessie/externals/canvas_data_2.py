b26b7c71e07c03fa51701605215379a15ac33bd0
sandeep markondiah jayaprakash <sandeepmarkondiahjayaprakash@ras-18-195.vpn.berkeley.edu>
2024-04-10 15:24:36 -0400

    [NS-1575] Canavs Data 2 Data Access Platform API authentication

---
+++ nessie/externals/canvas_data_2.py
@@ -0,0 +1,60 @@
+"""
+Copyright ©2024. The Regents of the University of California (Regents). All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its documentation
+for educational, research, and not-for-profit purposes, without fee and without a
+signed licensing agreement, is hereby granted, provided that the above copyright
+notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions.
+
+Contact The Office of Technology Licensing, UC Berkeley, 2150 Shattuck Avenue,
+Suite 510, Berkeley, CA 94720-1620, (510) 643-7201, otl@berkeley.edu,
+http://ipira.berkeley.edu/industry-info for commercial licensing opportunities.
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL,
+INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF
+THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS BEEN ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
+"AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ENHANCEMENTS, OR MODIFICATIONS.
+"""
+
+from base64 import b64encode
+
+from flask import current_app as app
+from nessie.externals import secrets_manager
+import requests
+
+
+def get_cd2_secret():
+    secret = secrets_manager.get_secret(app.config['CD2_SECRET_NAME'])
+    return secret
+
+
+def get_cd2_access_token():
+    secret = get_cd2_secret()
+    creds = f'{secret["DAP_CLIENT_ID"]}:{secret["DAP_CLIENT_SECRET"]}'
+    header = {'Authorization': 'Basic ' + b64encode(creds.encode()).decode()}
+    auth_url = f'{secret["DAP_API_URL"]}/ids/auth/login'
+    body = {'grant_type': 'client_credentials'}
+    # Request access token from the API
+    response = requests.post(auth_url, headers=header, data=body)
+    response_data = response.json()
+    access_token = response_data.get('access_token')
+
+    return access_token
+
+
+def get_cd2_tables_list(namespace):
+    secret = get_cd2_secret()
+    access_token = get_cd2_access_token()
+    headers = {'x-instauth': access_token}
+    # Get a list of available tables for the namespace. Namespace values can be 'canvas', 'catalog', 'canvas_logs'
+    cd2_tables_list = requests.get(f'{secret["DAP_API_URL"]}/dap/query/{namespace}/table', headers=headers)
+    app.logger.info(f'Tables Available for {namespace}: {len(cd2_tables_list.json()["tables"])}')
+
+    return cd2_tables_list.json()['tables']
