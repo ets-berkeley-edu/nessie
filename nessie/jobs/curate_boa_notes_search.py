d226f8b208b653d8d878343f83bb6bd89da49b9b
Anne Lyttle <alyttle@berkeley.edu>
2026-07-16 12:29:44 -0700

    NS-1798: replaces CalNet with EDL+EDW sources in BOA note search curation

--- nessie/jobs/curate_boa_notes_search.py
+++ nessie/jobs/curate_boa_notes_search.py
@@ -25,7 +25,7 @@ ENHANCEMENTS, OR MODIFICATIONS.
 
 from flask import current_app as app
 
-from nessie.externals import calnet, rds
+from nessie.externals import rds
 from nessie.jobs.background_job import BackgroundJob, BackgroundJobError
 from nessie.jobs.refresh_boa_app_rds_data_schema import RefreshBoaAppRdsDataSchema
 from nessie.lib.util import get_redshift_schema_boa_app_rds_data, resolve_sql_template
@@ -37,12 +37,10 @@ class CurateBoaNotesSearch(BackgroundJob):
 
     def run(self):
         app.logger.info('Starting Daily BOA Advising Notes Search Curation job...')
-        app.logger.info('Executing SQL...')
         RefreshBoaAppRdsDataSchema().run()
+        app.logger.info('Executing SQL...')
         self.create_schema()
-        self.import_note_authors()
         self.index_advising_notes()
-
         return 'BOA Advising Notes Search Curation job completed.'
 
 
@@ -57,49 +55,9 @@ class CurateBoaNotesSearch(BackgroundJob):
             raise BackgroundJobError('BOA App RDS Data Advising Notes RDS schema and index creation job failed.')
 
 
-    def import_note_authors(self):
-        advising_notes_schema = app.config['RDS_SCHEMA_ADVISING_NOTES']
-
-        app.logger.info('Fetching BOA advising note author attributes...')
-        advisor_attributes = self._advisor_attributes_by_uid()
-        if not advisor_attributes:
-            raise BackgroundJobError('Failed to fetch BOA advising note author attributes.')
-
-        unique_advisor_attributes = list({adv['uid']: adv for adv in advisor_attributes}.values())
-
-        with rds.transaction() as transaction:
-            insertable_rows = []
-            for entry in unique_advisor_attributes:
-                first_name, last_name = calnet.split_sortable_name(entry)
-                insertable_rows.append(tuple((entry.get('uid'), entry.get('csid'), first_name, last_name, entry.get('campus_email'))))  # noqa: C409
-
-            result = transaction.insert_bulk(
-                f"""
-                    INSERT INTO {advising_notes_schema}.advising_note_authors (uid, sid, first_name, last_name, campus_email)
-                    VALUES %s
-                    ON CONFLICT DO NOTHING
-                """,
-                insertable_rows,
-            )
-            if result:
-                transaction.commit()
-                app.logger.info('Imported BOA advising note author attributes.')
-            else:
-                transaction.rollback()
-                raise BackgroundJobError('Failed to import BOA advising note author attributes.')
-
     def index_advising_notes(self):
         resolved_ddl = resolve_sql_template('index_boa_notes_search_curation.template.sql')
         if rds.execute(resolved_ddl):
             app.logger.info('Indexed BOA advising notes.')
         else:
             raise BackgroundJobError('Failed to index BOA advising notes.')
-
-
-    def _advisor_attributes_by_uid(self):
-        boa_notes_schema = app.config['RDS_SCHEMA_BOA_APP_RDS_DATA']
-        advisor_uids_from_notes = set(
-            [r['advisor_uid'] for r in rds.fetch(f'SELECT DISTINCT advisor_uid FROM {boa_notes_schema}.advising_notes_nightly')],
-        )
-        advisor_uids = list(advisor_uids_from_notes)
-        return calnet.client(app).search_uids(advisor_uids)
